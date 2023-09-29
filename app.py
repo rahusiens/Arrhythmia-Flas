d3e0ab02fc24635b873ae0a3ad612ab64b0e045a
rahusiens <rahusiens@gmail.com>
2023-09-29 13:27:19 +0700

    add send_data function

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import os
 import pickle
 import classification
 import json
+import requests
 
 UPLOAD_FOLDER = os.path.join('staticFiles', 'uploads')
 ALLOWED_EXTENSION = {'csv'}
@@ -24,16 +25,28 @@ def hello_world():
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        lead1 = request.form["ekg1"]
-        lead2 = request.form["ekg2"]
+        id = request.form["patient_id"]
+        lead1 = request.form["lead1"]
+        lead2 = request.form["lead2"]
+        
+        lead1 = json.loads(lead1)
+        lead2 = json.loads(lead2)
 
-        sample_time = ["00:00:0" + str("%.3f" % round(i * 0.004, 3)) for i in range(len(json.loads(lead2)))]
+        sample_time = ["00:00:0" + str("%.3f" % round(i * 0.004, 3)) for i in range(len(lead2))]
 
-        dict = {'time': sample_time, 'lead1': json.loads(lead1), 'lead2': json.loads(lead2)}
+        dict = {'time': sample_time, 'lead1': lead1, 'lead2': lead2}
         df = pd.DataFrame(dict)
 
         feature = classification.cek(df)
         result = model.predict(feature)
+
+        r = requests.post("http://127.0.0.1:8000/api/send_data",
+            data={'patient_id': id,
+                    'time': str(sample_time),
+                    'lead1': str(lead1),
+                    'lead2': str(lead2)
+            })
+
         return json.dumps({"hasil": str(result)})
     
 if __name__ == '__main__':
