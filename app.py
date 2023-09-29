a54c486e15229ea6f4a6037179571a4bdad87c2a
rahusiens <rahusiens@gmail.com>
2023-09-29 18:44:10 +0700

    change url

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def predict():
         feature = classification.cek(df)
         result = model.predict(feature)
 
-        r = requests.post("http://https://aritmia.telekardiologi.com/api/send_data",
+        r = requests.post("https://aritmia.telekardiologi.com/api/send_data",
             data={'patient_id': id,
                     'time': str(sample_time),
                     'lead1': str(lead1),
